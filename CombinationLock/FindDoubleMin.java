cd4ffbddd3a5debd8cbc8dd48fc2ead94c5f59df
spyro21 <38793743+spyro21@users.noreply.github.com>
2020-02-12 14:38:43 -0600

    Create FindDoubleMin.java

---
+++ CombinationLock/FindDoubleMin.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class FindDoubleMin {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		double[] testArr = new double[10];
+		System.out.println("Enter 10 numbers: ");
+		
+		for(int i = 0; i < 10;i++) {
+			testArr[i] = input.nextDouble();
+		}
+		
+		System.out.println("the minimum number is: " + minDouble(testArr));
+		
+	}
+	
+	public static double minDouble(double[] arr) {
+		double value = arr[0];
+		for(int i = 1;i < arr.length;i++) {
+			if(arr[i] < value) {
+				value = arr[i];
+			}
+		}
+		return value;
+	}
+
+}
